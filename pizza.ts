e9d7551c7ea743912b50a57c72a8008718342f3b
Mansour Mbengue <mbgmansour13@gmail.com>
2025-06-12 10:41:42 +0200

    fix: implement TypeScript version of pizza app with order management , correct order ID management and remove redundant comments

--- pizza.ts
+++ pizza.ts
@@ -1,3 +1,8 @@
+type Pizza = {
+  name: string;
+  price: number;
+}
+
 const menu = [
   { name : "Margherita", price: 8 },
   { name : "Salmon", price: 15 },
@@ -6,34 +11,38 @@ const menu = [
   { name : "Parisian", price: 10 },
 ]
 
-const cashInRegister = 100;
-let nextOrderId = 1;
+let cashInRegister: number = 100;
+let nextOrderId: number = 1;
 const orderQueue = [];
 
-function addNewPizza(pizza) {
+function addNewPizza(pizza: Pizza) {
   menu.push(pizza);
 }
 
-function placeOrder(pizzaName) {
+function placeOrder(pizzaName: string) {
   const selectedPizza = menu.find(pizza => pizza.name === pizzaName);
+  if (!selectedPizza) {
+    console.error(`${pizzaName} does not exist in the menu`)
+    return;
+  }
   cashInRegister+= selectedPizza.price;
   const newOrder = { id: nextOrderId++ ,pizza: selectedPizza, status: "ordered" };
   orderQueue.push(newOrder);
   return newOrder
 }
 
-function completeOrder(orderId) {
-  const order = orderQueu.find(order.id === orderId);
+function completeOrder(orderId: number) {
+  const order = orderQueue.find(order.id === orderId);
   order.status = "completed";
   return order;
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
-addNewPizza({ name: "BBQ Chicken", cost: 12 })
-addNewPizza({ name: "Spicy Sausage", cost: 11 })
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
-completeOrder("1")
+completeOrder(1)
 
 console.log("Menu:", menu)
 console.log("Cash in register:", cashInRegister)
