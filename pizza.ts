146a7d5daf590160cd5afb16cd66240166551b39
Mansour Mbengue <mbgmansour13@gmail.com>
2025-06-16 23:55:40 +0200

    fix: refactor addNewPizza to use Omit utility type and return new pizza object; update user management functions

--- pizza.ts
+++ pizza.ts
@@ -24,10 +24,25 @@ const menu: Pizza[] = [
 
 const orderQueue: Order[] = [];
 
-function addNewPizza(pizza: Pizza) {
-  pizza.id = nextPizzaId++;
-  menu.unshift(pizza);
+/**
+ * Challenge:
+ * Fix the addNewPizza function using the Omit utility type. This might
+ * require more than just changing the "Pizza" typed `pizzaObj` parameter.
+ * Return the new pizza object (with the id added) from the function.
+ */
+
+function addNewPizza(pizza: Omit<Pizza, "id">): Pizza {
+  const newPizza: Pizza = { // Omit the id property from the input
+    // and assign a new id to the pizza object
+    id: nextPizzaId++, 
+    ...pizza
+  } 
+  menu.push(newPizza);
+  return newPizza; // Return the new pizza object
 }
+
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "Queen", price: 13 })
 
 function placeOrder(pizzaName: string) : Order | undefined { // added return type for clarity
   const selectedPizza = menu.find(pizza => pizza.name === pizzaName);
@@ -61,10 +76,54 @@ function getPizzaDetail(identifier: number | string) : Pizza | undefined {
   }
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ name: "Queen", price: 13 })
 // completeOrder(1)
 
 console.log("Menu:", menu)
 console.log("Cash in register:", cashInRegister)
 console.log("Order queue:", orderQueue)
+
+let userId : number = 1;
+
+type User = {
+  userId: number;
+  username: string;
+  role: "admin" | "user" | "guest";
+}
+
+const users : User[] = [{
+  userId: userId++,
+  username: "marcus",
+  role: "admin"
+}, {
+  userId: userId++,
+  username: "tim",
+  role: "user"
+}, {
+  userId: userId++,
+  username: "tom",
+  role: "guest"
+}]
+
+function updateUser(id: any, updates: any) {
+  const foundUser = users.find(user => user.userId === id);
+  if (!foundUser) {
+    console.error("User not found");
+    return;
+  }
+  Object.assign(foundUser, updates);
+}
+updateUser(userId++, {username:"Ibrahim"});
+updateUser(userId++, {role: "admin"});
+console.log("Users after update:", users);
+
+function addUser(newUser: Omit<User, "userId">): User {
+  const user: User = {
+    userId: userId++,
+    ...newUser
+  }
+  users.push(user);
+  return user;
+}
+
+addUser({ username: "newUser", role: "user" });
+console.log("Users before adding new user:", users)
